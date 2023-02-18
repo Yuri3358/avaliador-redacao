dbb884b58eca09a59852600bd824b24cdb373255
Yuri3358 <yurialmeida917@gmail.com>
2023-02-18 20:07:50 -0300

    feat(card): aboutme card ready

--- templates/presentationTemplate.js
+++ templates/presentationTemplate.js
@@ -1,13 +1,22 @@
 const cardTemplate = `
-    <div id="card-box">
-        <div id="username"></div>
-        <div id="infomatations">
-            <img height=100 width="100" src="./assets/creatoricon.jpeg"><img/>
-            Yuri3358
+        <div id="card-box">
+            <div id="picture">
+                <img id="username-picture" height=100 width="100" src="./assets/creatoricon.jpeg"><img/>        
+                <p id="username">Yuri3358</p>
+            </div>
             <hr/>
-            <a target="_blank" href="https://github.com/Yuri3358">Perfil do GitHub</a>
+            <div id="informations">
+                <label for="aboutme" class="labels">Sobre mim</label>
+                <p id="aboutme">Desenvolvedor com habilidades questionáveis, mas persistente.</p>
+                
+                <label for="contacts" class="labels">Social</label>
+                <p id="contacts">
+                    <a target="_blank" href="https://github.com/Yuri3358" class="profile-links">
+                        <i class="bi bi-github">Github</i>
+                    </a>
+                </p>
+            </div>
         </div>
-    </div>
 `
 
 exports = {cardTemplate}
