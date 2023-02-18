3a5780fe488a970664258079a2c661b2a4c31564
Yuri3358 <yurialmeida917@gmail.com>
2023-02-18 20:22:44 -0300

    feat(presentation): dynamic card infos

--- components/presentationComponent.js
+++ components/presentationComponent.js
@@ -1,5 +1,23 @@
 const cardComponent = {
-    template: cardTemplate
+    template: cardTemplate,
+    data() {
+        return {
+            creatorData: []
+        }
+    },
+    mounted() {
+        this.fetchData()
+    },
+    methods: {
+        fetchData() {
+            fetch("https://api.github.com/users/Yuri3358")
+            .then(res => res.json())
+            .then(output => {
+                this.creatorData = output
+                console.log(this.creatorData)
+            })
+        }
+    }
 }
 
 exports = {cardComponent}
--- templates/presentationTemplate.js
+++ templates/presentationTemplate.js
@@ -1,17 +1,17 @@
 const cardTemplate = `
         <div id="card-box">
             <div id="picture">
-                <img id="username-picture" height=100 width="100" src="./assets/creatoricon.jpeg"><img/>        
-                <p id="username">Yuri3358</p>
+                <img id="username-picture" height=100 width="100" :src="creatorData.avatar_url"><img/>        
+                <p id="username">{{ creatorData.login }}</p>
             </div>
             <hr/>
             <div id="informations">
                 <label for="aboutme" class="labels">Sobre mim</label>
-                <p id="aboutme">Desenvolvedor com habilidades questionáveis, mas persistente.</p>
+                <p id="aboutme">{{ creatorData.bio }}</p>
                 
                 <label for="contacts" class="labels">Social</label>
                 <p id="contacts">
-                    <a target="_blank" href="https://github.com/Yuri3358" class="profile-links">
+                    <a target="_blank" :href="creatorData.html_url" class="profile-links">
                         <i class="bi bi-github">Github</i>
                     </a>
                 </p>
