235d454f9ba291abdf76a5e492a599748536301a
Yuri3358 <yurialmeida917@gmail.com>
2023-02-19 10:14:18 -0300

    feat(forms): data inputs

--- templates/formsTemplate.js
+++ templates/formsTemplate.js
@@ -1,5 +1,27 @@
 const formsTemplate = `
-    <h1>This is the Register</h1>
+    <div id="form-box">
+        <h1>Registrar Redator</h1>
+        
+        <label for="name">Autor</label>
+        <input id="name" v-model="authorName">
+
+        <label for="DEF">Escrita formal</label>
+        <input id="DEF" v-model="def">
+
+        <label for="CT">Compreensão do Tema</label>
+        <input type="number" max="200" id="CT" v-model="ct">
+        
+        <label for="CR">Coerência do Texto</label>
+        <input type="number" max="200" id="CR" v-model="CR">
+
+        <label for="ML">Mecanismos Linguísticos</label>
+        <input type="number" max="200" id="ML" v-model="ML">
+
+        <label for="RDH">Direitos Humanos</label>
+        <input type="number" max="200" id="RDH" v-model="RDH">
+
+        <button class="save btn btn-success" @click="saveEssay">Registrar</button>
+    </div>
 `
 
 exports = {formsTemplate}
