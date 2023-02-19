a1a8ead0b60821033930a85b0e6bc196252cfd5a
Yuri3358 <yurialmeida917@gmail.com>
2023-02-19 12:03:55 -0300

    feat: fetchAuthors()

--- components/dataComponent.js
+++ components/dataComponent.js
@@ -1,5 +1,25 @@
 const dataComponent = {
-    template: dataTemplate
+    template: dataTemplate,
+    data() {
+        return {
+            authors: []
+        }
+    },
+    mounted() {
+        this.fetchAuthors()
+    },
+    methods: {
+        fetchAuthors() {
+            colRef.get().then(snapshot => {
+                snapshot.forEach(doc => {
+                    const authorsData = doc.data()
+                    authorsData.name = doc.id
+                    this.authors.push(authorsData)
+                    this.authors.sort((a, b) => a.name.localeCompare(b.name))
+                })
+            })
+        }
+    }
 }
 
 exports = {dataComponent}
