67f804ffff175828d0e8c1c40060ef1c8cf00b58
Yuri3358 <yurialmeida917@gmail.com>
2023-02-19 12:03:36 -0300

    feat: table

--- templates/dataTemplate.js
+++ templates/dataTemplate.js
@@ -1,5 +1,27 @@
 const dataTemplate = `
-    <h1>That are the Essays </h1>
+    <h1 id="authors-header">Lista de Redatores</h1>
+    <table class="table">
+        <thead>
+            <th>Autor</th>
+            <th>Domínio Escrita Formal</th>
+            <th>Compreensão do Tema</th>
+            <th>Coerência Textual</th>
+            <th>Mecanismos Linguísticos</th>
+            <th>Respeito Direitos Humanos</th>
+        </thead>
+
+        <tbody>
+            <tr v-for="author in this.authors">
+                <td>{{ author.name }}</td>
+                <td>{{ author.escrita_formal }}</td>
+                <td>{{ author.compreensao_tema }}</td>
+                <td>{{ author.coerencia_redacao }}</td>
+                <td>{{ author.mecanismos_linguisticos }}</td>
+                <td>{{ author.direitos_humanos }}</td>
+            </tr>
+        
+        </tbody>
+    </table>
 `
 
 exports = {dataTemplate}
