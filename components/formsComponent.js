1f9fc38948b36b19edd36cacbc5a23e549d1d5ab
Yuri3358 <yurialmeida917@gmail.com>
2023-02-19 11:22:16 -0300

    feat: register author

--- components/formsComponent.js
+++ components/formsComponent.js
@@ -1,5 +1,34 @@
 const formsComponent = {
-    template: formsTemplate
+    template: formsTemplate,
+    data() {
+        return {
+            authorName: "",
+            def: 0, 
+            ct: 0,
+            cr: 0,
+            ml: 0,
+            rdh: 0
+        }
+    },
+    methods: {
+        async saveEssay() {
+            let variables = [this.def, this.ct, this.cr, this.ml, this.rdh]
+            let parsedVariables = variables.map(variable => Number(variable))
+
+            let variablesSum = parsedVariables.reduce((acc, current) => acc + current)
+            
+            await colRef.doc(this.authorName).set({
+                escrita_formal: this.def,
+                compreensao_tema: this.ct,
+                coerencia_redacao: this.cr,
+                mecanismos_linguisticos: this.ml,
+                direitos_humanos: this.rdh,
+                media_final: variablesSum
+            })
+            this.def = this.ct = this.cr = this.ml = this.rdh = 0, 
+            this.authorName = ""
+        }
+    }
 }
 
 exports = {formsComponent}
