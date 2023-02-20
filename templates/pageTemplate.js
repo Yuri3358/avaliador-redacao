6f615d78f5958557da65ea46de367f2de70eb593
Yuri3358 <yurialmeida917@gmail.com>
2023-02-20 19:30:46 -0300

    style(fix): navbars more centralized

--- templates/pageTemplate.js
+++ templates/pageTemplate.js
@@ -1,7 +1,7 @@
 const pageTemplate = `
     <header>
         <ul>
-            <h2 id="brand">Avaliador ENEM</h2>
+            <h3 id="brand">Avaliador ENEM</h3>
             <p class="links">
                 <a class="btn btn-outline anchors" href="#/lista">Redatores</a>
             </p>
